f25c74268e1a645043953cefd1033ff6b5cf1faa
carllosaguiar <carlosaguiar23@gmail.com>
2020-09-21 22:00:20 -0300

    creating file of crud

--- src/routes/plants/index.js
+++ src/routes/plants/index.js
@@ -7,11 +7,40 @@ route.get('/', function(req, res){
     res.render('home', { title : 'CRUD | With Node.js'});
 })
 
-// plants
-route.post('/', function(req, res){
-    console.log(req.body)
-    res.render('home', { title : 'CRUD | With Node.js'});
+
+// Crud of plants
+
+// List all plants.
+route.get('/plants', function(req, res){
+    res.render('plants', { title : 'CRUD | Show Plants'})
 })
 
+// Added a new title
+route.get('/plants/new', function(req, res){
+    res.render('plants/new.ejs', { title : 'CRUD | Added new Title'})
+})
+
+// Update a title
+route.get('/plants/update', function(req, res){
+    res.render('plants/update.ejs', { title : 'CRUD | Update Title'})
+})
+
+// Delete a title
+route.get('/plants/remove', function(req, res){
+    res.render('plants/remove.ejs', { title : 'CRUD | Remove Title'})
+})
+
+
+// Create a new title in database
+
+route.post('/add', function(req, res){
+    console.log(req.body)
+    res.render('plants', { title : 'CRUD | With Node.js'});
+})
+
+route.put('/change', function(req, res){
+    console.log(req.body)
+    res.render('plants', { title : 'CRUD | With Node.js'})
+})
 
 module.exports = route;
